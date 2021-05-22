c856fe48d9676cd8aa065b1ce00fa5e39d4d9829
pfernandes21 <pedro.miguel.fernandes@tecnico.ulisboa.pt>
2021-05-23 00:16:21 +0100

    updated coap msg repetition

--- bc66/bc66.ino
+++ bc66/bc66.ino
@@ -4,43 +4,80 @@
 
 nbUDP U;
 Coap coap(U);
+int t = 0;
+String msg = "";
 
 void callback_response(CoapPacket &packet, IPAddress ip, int port) {
   char p[packet.payloadlen + 1];
   memcpy(p, packet.payload, packet.payloadlen);
   p[packet.payloadlen] = NULL;
   Serial.print(p);
+  msg = "";
 }
-
-//void send_cmd(char *CMD) {
-//  s32 ret = 1;
-//  ret = Ql_RIL_SendATCmd(CMD, strlen(CMD), callback, NULL, DEFAULT_AT_TIMEOUT);
-//}
-//
-//s32 callback(char* line, u32 len, void* userdata)
-//{
-//  Serial.println(line);
-//}
 
 void setup()
 {
+  t = millis();
+  String imei, mcc_mnc, sim_imsi, sim_iccid, uid;
   Dev.noSleep();
-  Serial.begin(9600);
+
+  Serial.begin(115200);
+
+  Serial.printf("Arduino      %s\n", Dev.getVersion());
+  Dev.getImei(imei);
+  Serial.printf("IMEI         %s\n", imei.c_str());
+  Dev.getUid(uid);
+  Serial.printf("UID          %s\n", uid.c_str());
+
   Dev.waitSimReady();
-  Serial.println("SIM ready");
+
+  Dev.getMccMnc(mcc_mnc);
+  Serial.printf("MCCMNC       %s\n", mcc_mnc.c_str());
+  Dev.getImsi(sim_imsi);
+  Serial.printf("IMSI         %s\n", sim_imsi.c_str());
+  Dev.getIccid(sim_iccid);
+  Serial.printf("ICCID        %s\n", sim_iccid.c_str());
+
   Dev.waitCereg();
-  Serial.println("Net ready");
+  delay(200);
+
+  Serial.printf("Rx level     %d dbm\n", Dev.getReceiveLevel());
+  Serial.printf("Rx quality   %d\n", Dev.getQuality());
+  Serial.printf("Rx access    %d\n", Dev.getAccess());
+  Serial.printf("Cell cid     %d\n", Dev.getCid());
+  Serial.printf("Cell tac     %d\n", Dev.getTac());
+  char mlts[322];
+  Dev.getMlts(mlts, 322);
+  Serial.printf("Cell mlts    %s\n", mlts);
+  Serial.printf("Elapsed: %d mili seconds\n", millis() - t);
+
   delay(200);
   coap.response(callback_response);
   coap.start();
-  Serial.println("ready");
+  Serial.println("Ready");
+  delay(200);
 }
 
-void loop() {
-  if (Serial.available()) {
-    String msg = Serial.readString();
+void loop()
+{
+  if (Serial.available() > 0) {
+    msg = Serial.readString();
+    int nindex = msg.lastIndexOf('\n', msg.length() - 2);
+    msg = msg.substring(nindex > -1 ? nindex : 0);
+    msg.trim();
+    Serial.println("Got " + msg);
     coap.put(IPAddress(85, 246, 38, 211), 5683, "lys", msg.c_str());
-    delay(2000);
+    Serial.println("sending " + msg);
+    t = millis();
   }
+
+  if (msg.length() > 0 && millis() > t + 60000) {
+    //coap.put(IPAddress(146, 193, 41, 162), 5683, "lys", msg.c_str());
+    coap.put(IPAddress(85, 246, 38, 211), 5683, "lys", msg.c_str());
+    Serial.println("sending " + msg);
+    t = millis();
+  }
+
+  delay(4000);
   coap.loop();
 }
